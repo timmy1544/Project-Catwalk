e33bf10a7da5999820d6dac549afb6bfbf0de5cf
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2021-10-26 16:20:00 -0700

    successfully got api  data using axios

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,13 +3,39 @@ import ProductDetails from './ProductDetails/ProductDetails.jsx';
 import QandA from './QandA/QandA.jsx';
 import RelatedProducts from './RelatedProducts/RelatedProducts.jsx';
 import Reviews from './Reviews/Reviews.jsx';
+import config from '../config/config.js';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {}
+
+    this.getProducts = this.getProducts.bind(this);
+  }
+
+
+  componentDidMount() {
+    this.getProducts()
+
+
+  }
+
+  getProducts() {
+    axios.get(`${config.ALTELIER_API}/products`, {
+      headers: {
+        'Authorization': `${config.API_KEY}`
+      }
+    })
+    .then((res) => {
+      console.log('data', res.data);
+    })
+    .catch(err => console.log('error', err));
   }
 
   render() {
+    console.log(config)
     return (
       <div>
         <p>Hello Cat Trap</p>
