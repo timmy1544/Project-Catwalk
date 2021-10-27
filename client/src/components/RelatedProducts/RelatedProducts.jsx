ccc24df910c5608774c90bffe435a9d52c6cb5bc
NicholasAnich <infinitevolume@gmail.com>
2021-10-26 17:13:09 -0700

    accept pull request test plx

--- client/src/components/RelatedProducts/RelatedProducts.jsx
+++ client/src/components/RelatedProducts/RelatedProducts.jsx
@@ -7,7 +7,25 @@ class RelatedProducts extends React.Component {
 
   render() {
     return (
-      <div>Related Products here :D</div>
+      <div>
+        <select>
+          <option>OH WOW SOOOO COOOL</option>
+          <option>OH WOW SOOOO COOOL</option>
+          <option>OH WOW SOOOO COOOL</option>
+          <option>OH WOW SOOOO COOOL</option>
+          <option>OH WOW SOOOO COOOL</option>
+        </select>
+
+        <div>Related Products here :D</div>
+        <div>Cool jacket here</div>
+        <div>Would you like dis related product</div>
+        <div>Would you like dis related A</div>
+        <div>Would you like dis related B</div>
+        <div>Would you like dis related C</div>
+        <div>Would you like dis related D</div>
+        <div>Would you like dis related E</div>
+        <div>Would you like dis related F</div>
+      </div>
     )
   }
 }
