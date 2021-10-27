0071f1bfc01cc89ed239d85bf0474025e8f827d5
Rufus Roffie <jinbl12go@gmail.com>
2021-10-27 11:25:42 -0700

    testing

--- client/src/components/ProductDetails/ProductDetails.jsx
+++ client/src/components/ProductDetails/ProductDetails.jsx
@@ -3,13 +3,57 @@ import React from 'react';
 class ProductDetails extends React.Component{
   constructor(props) {
     super(props)
+    super(props);
+    this.state = {
+      productsData: null
+    }
+    this.getProducts = this.getProducts.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts() {
+    axios.get(`${config.ALTELIER_API}/products`, {
+      headers: {
+        'Authorization': `${config.API_KEY}`
+      }
+    })
+    .then((res) => {
+      //console.log('data', res.data);
+      this.setState({
+        productsData: res.data
+      })
+    })
+    .catch(err => {
+      console.log('error', err)
+    });
   }
 
   render() {
+    if(!this.state.productsData) {return null;}
     return (
       <div>Product Detail</div>
+      <div>
+        Product Details
+        <ul>
+      {this.state.productsData.map((item, index) => {
+        return <Product item={item} key={index}/>;
+      })}
+      </ul>
+      </div>
     );
   }
 }
 
-export default ProductDetails;
+const Product = (props) => {
+  return (
+    <li>
+      <span>{props.item.name}</span>
+      <span>{props.item.id}</span>
+    </li>
+  )
+}
+
+export default ProductDetails; 
--- config.example.js
+++
@@ -1,5 +0,0 @@
-// Put your API key goes here!
-var API_KEY = 'YOUR_API_KEY_HERE';
-var ALTELIER_API = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/';
-
-export { API_KEY, ALTELIER_API };
