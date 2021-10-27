35612746398c76f98c5aa9ae49fd3be56e14f577
Timmy Liaw <timmy1544@gmail.com>
2021-10-26 17:04:14 -0700

    merging test - git ignore works please

--- client/src/config/config.js
+++
@@ -1,5 +0,0 @@
-// Put your API key goes here!
-var API_KEY = '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';
-var ALTELIER_API = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax';
-
-module.exports = { API_KEY, ALTELIER_API };
