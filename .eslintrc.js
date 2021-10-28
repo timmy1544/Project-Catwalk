1889a2629d8ff04085978746dbb5e19d31b3f7ac
Timmy Liaw <timmy1544@gmail.com>
2021-10-28 14:47:21 -0700

    fix eslint rule - less stricts

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,15 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    // 'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-console': 'off',
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: "CallExpression[callee.object.name='console'][callee.property.name!=/^(log|warn|error|info|trace)$/]",
+        message: 'Unexpected property on console object was called',
+      },
+    ],
+    'eol-last': 0,
   },
 };
--- server/index.js
+++ server/index.js
@@ -9,16 +9,14 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('port', 3000);
 
 app.listen(app.get('port'), (err) => {
-  if(err) throw err;
+  if (err) throw err;
   console.log('listening to port 3000');
 });
-
-
 
 // app.listen('port', () => {
 //   console.log(`Listening on port: ${port}`)
