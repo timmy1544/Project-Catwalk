5377dd72844c28851be9c3f851b7efe1d5c71049
NicholasAnich <infinitevolume@gmail.com>
2021-11-01 16:20:38 -0700

    created children components for RelatedProducts component

---
+++ client/src/components/RelatedProducts/OutFitList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const OutFitList = () => (
+
+  <div className="outfit">
+    <h2>Outfit here</h2>
+    <h2>Outfit here</h2>
+    <h2>Outfit here</h2>
+    <h2>Outfit here</h2>
+  </div>
+);
+
+
+export default OutFitList;
---
+++ client/src/components/RelatedProducts/ProductLineList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ProductLineList = () => (
+
+  <div className="related-products">
+    <h2>Product here</h2>
+    <h2>Product here</h2>
+    <h2>Product here</h2>
+    <h2>Product here</h2>
+  </div>
+);
+
+
+export default ProductLineList;
