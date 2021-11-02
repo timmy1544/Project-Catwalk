cd16834906052bc56b1887635dd5cef8e3b7e798
Rufus Roffie <jinbl12go@gmail.com>
2021-11-01 17:19:58 -0700

    made changes to productdetail component

--- client/src/components/ProductDetails/productStyle.jsx
+++ client/src/components/ProductDetails/productStyle.jsx
@@ -1,8 +1,22 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 import React from 'react';
 
-const ProductStyle = () => (
+const ProductStyle = (props) => (
   <div>
-    this is style section
+    <Photos />
+    <div className="styels">
+      <div className="review">review goes here!</div>
+      <div>{props.product.category}</div>
+      <div>{props.product.name}</div>
+
+    </div>
+  </div>
+);
+
+const Photos = () => (
+  <div>
+    photoGallery
   </div>
 );
 
