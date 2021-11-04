c7e69d3d1ced564d9ad949775629aef0c46658e6
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2021-11-04 12:01:45 -0700

    dynamically rendering questions

--- client/src/components/QandA/QandAEntry.jsx
+++ client/src/components/QandA/QandAEntry.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable eol-last */
 import React from 'react';
 import Question from './Question';
@@ -10,10 +12,20 @@ class QandAEntry extends React.Component {
   }
 
   render() {
+    const { questions } = this.props;
+    // console.log('questions entry', questions);
+    let quest;
+    if (questions.length === 0) {
+      quest = <Question />;
+    } else {
+      quest = questions.map((question, index) => (<Question question={question} key={index} />));
+    }
     return (
       <div>
         Q:
-        <Question />
+        {/* {questions.map((question, index) =>
+          <Question question={question.question_body} key={index} />)} */}
+        {quest}
         A:
         <Answer />
       </div>
--- client/src/components/QandA/Question.jsx
+++ client/src/components/QandA/Question.jsx
@@ -1,5 +1,8 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 /* eslint-disable eol-last */
 import React from 'react';
+import moment from 'moment';
 
 class Question extends React.Component {
   constructor(props) {
@@ -8,9 +11,26 @@ class Question extends React.Component {
   }
 
   render() {
+    let questionBody;
+    let askerName;
+    let questionDate;
+    let convertedDate;
+    if (this.props.question) {
+      questionBody = this.props.question.question_body;
+      askerName = this.props.question.asker_name;
+      questionDate = this.props.question.question_date;
+      convertedDate = moment(questionDate).format('MMMM Do[,] YYYY');
+      // console.log('question info', questionBody, askerName, convertedDate);
+    } else {
+      questionBody = 'No Question Data';
+      askerName = '';
+      convertedDate = '';
+    }
     return (
       <div>
-        Question  Helpful | Add Answer
+        {questionBody}
+        <br />
+        {`${askerName}, ${convertedDate}`}
       </div>
     );
   }
