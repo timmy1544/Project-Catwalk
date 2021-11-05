8357938a86ae4ade40a4797d49afc09721cc5ad0
Teresa Nguyen <88606876+teresaanguyen@users.noreply.github.com>
2021-11-04 18:55:13 -0700

    rendering answers

--- client/src/components/QandA/Answer.jsx
+++ client/src/components/QandA/Answer.jsx
@@ -1,65 +1,27 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 /* eslint-disable eol-last */
 import React from 'react';
 import moment from 'moment';
-import axios from 'axios';
 
 class Answer extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      answers: [],
     };
-    if (this.props.questionID) {
-      const questionID = this.props.questionID;
-    }
   }
 
-  // componentDidMount() {
-  //   this.getAnswers();
-  // }
-
-  // getAnswers() {
-  //   if (this.props.questionID) {
-  //     console.log('first ID', this.props.questionID);
-  //     const questionID = this.props.questionID;
-  //     // console.log('first ID', questionID);
-  //     axios.get(`/qa/${questionID}/answers`)
-  //       .then((response) => {
-  //         console.log('second ID', questionID);
-  //         console.log('inside axios questionid answers', response.data.results);
-  //         this.setState({
-  //           answers: response.data.results,
-  //         });
-  //       });
-  //   }
-  // }
-
   render() {
-    let answerObj;
-    let answerBody;
-    let answerName;
-    let answerDate;
-    let answerConvertedDate;
-    // if (this.props.answer) {
-    //   answerObj = this.props.answer;
-    //   answerBody = answerObj.body;
-    //   answerName = answerObj.answerer_name;
-    //   answerDate = answerObj.date;
-    //   answerConvertedDate = moment(answerDate).format('MMMM Do[,] YYYY');
-    //   console.log('converted date answer', answerConvertedDate);
-    // } else {
-    //   answerObj = {};
-    //   answerBody = '';
-    //   answerName = '';
-    //   answerConvertedDate = '';
-    // }
+    const answer = this.props.answerObj;
+    const { date } = answer;
+    const answerConvertedDate = moment(date).format('MMMM Do[,] YYYY');
     return (
       <div>
-        {answerBody}
+        {answer.body}
         <br />
         by
         {' '}
-        {answerName}
+        {answer.answerer_name}
         ,
         {' '}
         {answerConvertedDate}
--- client/src/components/QandA/Question.jsx
+++ client/src/components/QandA/Question.jsx
@@ -1,40 +1,41 @@
+/* eslint-disable react/no-array-index-key */
 /* eslint-disable react/destructuring-assignment */
 /* eslint-disable react/prop-types */
 /* eslint-disable eol-last */
 import React from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import Answer from './Answer';
 
 class Question extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      answers: [],
+      // answers: [],
     };
-    console.log('question component prop:', this.props);
   }
 
   render() {
     let questionBody;
-    let questionID;
+    let answersArray;
     if (this.props.question) {
       questionBody = this.props.question.question_body;
-      questionID = this.props.question.question_id;
-      // console.log('questionID', questionID);
+      // const answerIDs = Object.keys(this.props.question.answers);
+      answersArray = Object.values(this.props.question.answers);
     } else {
       questionBody = 'No Question Data';
-      questionID = '';
+      answersArray = [];
     }
     return (
       <div>
         Q:
-        {questionBody}
+        <div>
+          {questionBody}
+        </div>
         <br />
         A:
-        <Answer questionID={questionID} />
-        {/* {this.state.answers.map((answer, index) => (
-          <Answer answer={answer} key={index} />
-        ))} */}
+        {answersArray.map((answerObj, index) => (
+          <Answer answerObj={answerObj} key={index} />
+        ))}
       </div>
     );
   }
