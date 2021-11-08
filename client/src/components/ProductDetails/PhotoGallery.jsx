314f56b5b9eac5e8d0459f6b43d590afbc4e4311
Rufus Roffie <jinbl12go@gmail.com>
2021-11-08 11:26:27 -0800

    added arrow to thumbmail slider

--- client/src/components/ProductDetails/PhotoGallery.jsx
+++ client/src/components/ProductDetails/PhotoGallery.jsx
@@ -2,25 +2,38 @@
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable react/no-array-index-key */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
+import React, { useState, useRef, useLayoutEffect } from 'react';
 import { BsArrowLeftCircle, BsArrowRightCircle } from 'react-icons/bs';
+import { TiArrowSortedDown, TiArrowSortedUp } from 'react-icons/ti';
 
 const PhotoGallery = ({ currentStyle }) => {
-  // console.log('currentStyle', currentStyle);
+  console.log('currentStyle', currentStyle);
   const [current, setCurrent] = useState(0);
   const [selectThumb, setSelectThumb] = useState('');
+  const [showArrow, setShowArrow] = useState(false);
+
   const slides = currentStyle.photos;
   const { length } = slides;
+  const ref = useRef(null);
   const selectThumbnail = (index) => {
     setSelectThumb(index);
   };
-  const nextPic = () => {
+  const nextPic = () => { // fix here: when its end of index, one side arrow will disappear
     setCurrent(current === length - 1 ? 0 : current + 1);
   };
   const prevPic = () => {
     setCurrent(current === 0 ? length - 1 : current - 1);
   };
-  // console.log('currentindex', current);
+  const scroll = (scrollOffset) => {
+    console.log(ref);
+    ref.current.scrollTop += scrollOffset;
+  };
+
+  useLayoutEffect(() => {
+    if (ref.current.clientHeight < ref.current.scrollHeight) {
+      setShowArrow(true);
+    }
+  }, [ref]);
 
   return (
     <div className="photoGallery">
@@ -31,20 +44,26 @@ const PhotoGallery = ({ currentStyle }) => {
           {index === current && (<img src={item.url} alt="pic" className="photo" />)}
         </div>
       ))}
-      <div className="slider">
-        {slides.map((item, index) => (
-          <div className={index === selectThumb ? 'selectedThumbnailPic' : 'thumbnailPic'} key={index}>
-            <img
-              src={item.thumbnail_url}
-              alt="thumbnail"
-              className="thumbnail"
-              onClick={() => {
-                setCurrent(index);
-                selectThumbnail(index);
-              }}
-            />
-          </div>
-        ))}
+      <div className="sliderWrapper">
+        {showArrow && <TiArrowSortedUp onClick={() => scroll(-20)} className="upArrow" />}
+        <div ref={ref} className="slider">
+          {slides.map((item, index) => (
+            <div className={index === selectThumb || index === current ? 'selectedThumbnailPic' : 'thumbnailPic'} key={index}>
+              <img
+                src={item.thumbnail_url}
+                alt="thumbnail"
+                className="thumbnail"
+                onClick={() => {
+                  setCurrent(index);
+                  selectThumbnail(index);
+                }}
+              />
+            </div>
+          ))}
+          {/* <div class="thumbnailPic"><img src="https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=300&amp;q=80" alt="thumbnail" class="thumbnail"></img></div>
+          <div class="thumbnailPic"><img src="https://images.unsplash.com/photo-1534011546717-407bced4d25c?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=300&amp;q=80" alt="thumbnail" class="thumbnail"></img></div> */}
+        </div>
+        {showArrow && <TiArrowSortedDown onClick={() => scroll(20)} className="downArrow" />}
       </div>
     </div>
   );
