f430db6d4051aa01a3a9e5fc62c52a1f802cb59c
NicholasAnich <infinitevolume@gmail.com>
2021-11-09 23:26:56 -0800

    implemented Modal Component

--- client/src/components/RelatedProducts/RelatedProduct_Components/ModalTable.jsx
+++ client/src/components/RelatedProducts/RelatedProduct_Components/ModalTable.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 /* eslint-disable react/no-array-index-key */
 import React, { useState, useEffect } from 'react';
 import CheckIcon from '@mui/icons-material/Check';
@@ -5,48 +6,79 @@ import CheckIcon from '@mui/icons-material/Check';
 const ModalTable = ({
   features, mainFeatures, productObj, relatedProductsObj,
 }) => {
+  const [compareFeatures, setCompareFeatures] = useState([]);
+
   const style = {
     textAlign: 'center',
     wordWrap: 'break-word',
     whiteSpace: 'normal',
   };
 
+  const checkColor = {
+    color: 'green',
+  };
+
   const divSize = {
-    // tableLayout: 'fixed',
     width: '100%',
   };
 
-  const mapFeatures = features.map((item, i) => (
-    <tr key={`modal-table-feature-${i}`} style={style}>
-      <td><CheckIcon /></td>
-      <td>
-        {
-          item.feature
-            && item.value
-            ? `~${item.feature}~ \n
-
-          ${item.value}` : item.feature
-        }
-      </td>
-      <td><CheckIcon /></td>
-    </tr>
-  ));
-
-  const mapMainFeatures = mainFeatures.map((item, i) => (
+  const tableFeatures = compareFeatures.map((item, i) => (
     <tr key={`modal-table-mainFeature-${i}`} style={style}>
-      <td><CheckIcon /></td>
+      <td style={checkColor}>{item.main ? <CheckIcon /> : null}</td>
       <td>
-        {
-          item.feature
-            && item.value
-            ? `~${item.feature}~ \n
-
-          ${item.value}` : item.feature
-        }
+        {item.value}
       </td>
-      <td><CheckIcon /></td>
+      <td style={checkColor}>{item.compared ? <CheckIcon /> : null}</td>
     </tr>
   ));
+
+  const addFeatures = (features, mainFeatures) => {
+    const reducedFeatures = [];
+    const comparedHasFeat = [];
+    const sameFeatures = [];
+
+    features.forEach(item => {
+      if (reducedFeatures.indexOf(item.value) === -1 && item.value !== null) {
+        reducedFeatures.push(item.value);
+        comparedHasFeat.push({
+          value: item.value,
+          compared: true,
+          main: false,
+        });
+      }
+    });
+
+    mainFeatures.forEach(item => {
+      if (reducedFeatures.indexOf(item.value) === -1 && item.value !== null) {
+        reducedFeatures.push(item.value);
+        comparedHasFeat.push({
+          value: item.value,
+          compared: false,
+          main: true,
+        });
+      } else if (reducedFeatures.indexOf(item.value) >= 0 && item.value !== null) {
+        sameFeatures.push({
+          value: item.value,
+          compared: true,
+          main: true,
+        });
+      }
+    });
+
+    comparedHasFeat.forEach((item) => {
+      for (const key in sameFeatures) {
+        if (item.value === sameFeatures[key].value) {
+          item.main = true;
+        }
+      }
+    });
+
+    setCompareFeatures(comparedHasFeat);
+  };
+
+  useEffect(() => {
+    addFeatures(features, mainFeatures);
+  }, [features, mainFeatures]);
 
   return (
     <div style={divSize}>
@@ -61,114 +93,10 @@ const ModalTable = ({
           </tr>
         </thead>
         <tbody>
-          {mapFeatures}
-          {mapMainFeatures}
+          {tableFeatures}
         </tbody>
       </table>
     </div>
   );
 };
 export default ModalTable;
-
-// /* eslint-disable react/no-array-index-key */
-// import React, { useState, useEffect } from 'react';
-// import CheckIcon from '@mui/icons-material/Check';
-
-// const ModalTable = ({
-//   features, mainFeatures, productObj, relatedProductsObj,
-// }) => {
-//   // TO BE CONTINUED...
-//   // make a compare products method
-//   // refactor to be a table
-//   const mapFeatures = features.map((item, i) => (
-//     <div key={`modal-table-feature-${i}`} className="product-characteristics-container">
-//       <div className="whiteCheckIcon"><CheckIcon /></div>
-//       <p className="featureText">{item.feature && item.value ? `${item.feature}-${item.value}` : item.feature}</p>
-//       <div className="comparedCheckIcon"><CheckIcon /></div>
-//     </div>
-//   ));
-
-//   const mapMainFeatures = mainFeatures.map((item, i) => (
-//     <div key={`modal-table-mainFeature-${i}`} className="product-characteristics-container">
-//       <div className="mainCheckIcon"><CheckIcon /></div>
-//       <p className="featureText">{item.feature && item.value ? `${item.feature}-${item.value}` : item.feature}</p>
-//       <div className="whiteCheckIcon"><CheckIcon /></div>
-//     </div>
-//   ));
-
-//   // useEffect(() => {
-
-//   // }, [productObj, relatedProductsObj, features, mainFeatures])
-
-//   return (
-//     <div className="Modal-Container">
-//       <h1>Comparing</h1>
-//       <span className="headline-container">
-//         <h2 className="mainName">{productObj.name}</h2>
-//         <h2 className="featuresText">FEATURES</h2>
-//         <h2 className="relatedName">{relatedProductsObj.name}</h2>
-//       </span>
-//       <span className="table-list-container">
-//         {mapMainFeatures}
-//         {mapFeatures}
-//       </span>
-//     </div>
-//   );
-// };
-// export default ModalTable;
-
-//  TABLE IMPLEMENTATION
-// import * as React from 'react';
-// import Table from '@mui/material/Table';
-// import TableBody from '@mui/material/TableBody';
-// import TableCell from '@mui/material/TableCell';
-// import TableContainer from '@mui/material/TableContainer';
-// import TableHead from '@mui/material/TableHead';
-// import TableRow from '@mui/material/TableRow';
-// import Paper from '@mui/material/Paper';
-
-// function createData(name, calories, fat, carbs, protein) {
-//   return { name, calories, fat, carbs, protein };
-// }
-
-// const rows = [
-//   createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//   createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//   createData('Eclair', 262, 16.0, 24, 6.0),
-//   createData('Cupcake', 305, 3.7, 67, 4.3),
-//   createData('Gingerbread', 356, 16.0, 49, 3.9),
-// ];
-
-// const ModalTable = () => (
-//   <TableContainer component={Paper}>
-//     <Table sx={{ minWidth: 650 }} aria-label="simple table">
-//       <TableHead>
-//         <TableRow>
-//           <TableCell>Dessert (100g serving)</TableCell>
-//           <TableCell align="right">Calories</TableCell>
-//           <TableCell align="right">Fat&nbsp;(g)</TableCell>
-//           <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-//           <TableCell align="right">Protein&nbsp;(g)</TableCell>
-//         </TableRow>
-//       </TableHead>
-//       <TableBody>
-//         {rows.map((row) => (
-//           <TableRow
-//             key={row.name}
-//             sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-//           >
-//             <TableCell component="th" scope="row">
-//               {row.name}
-//             </TableCell>
-//             <TableCell align="right">{row.calories}</TableCell>
-//             <TableCell align="right">{row.fat}</TableCell>
-//             <TableCell align="right">{row.carbs}</TableCell>
-//             <TableCell align="right">{row.protein}</TableCell>
-//           </TableRow>
-//         ))}
-//       </TableBody>
-//     </Table>
-//   </TableContainer>
-// );
-
-// export default ModalTable
